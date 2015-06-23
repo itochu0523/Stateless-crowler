ab6e97384a8de5f65435f61e5df5b1effd531014
Takashi Sato <leone0279@gmail.com>
2015-06-24 02:35:22 +0900

    ベストセラー情報を取得する

--- scraping.rb
+++ scraping.rb
@@ -12,13 +12,27 @@ urls.push("http://www.amazon.co.jp/gp/bestsellers/books/466282/")
 Anemone.crawl(urls, depth_limit: 0) do |anemone|
   anemone.on_every_page do |page|
   # 文字コードをUTF-8に変換したうえで、Nokogiriでパース
-  doc = Nokogiri::HTML.parse(page.body.toutf8)
+    doc = Nokogiri::HTML.parse(page.body.toutf8)
 
-  category = doc.xpath("//*[@id='zg_browseRoot']/ul/li/a").text
+    category = doc.xpath("//*[@id='zg_browseRoot']/ul/li/a").text
 
-  # カテゴリの表示
-  sub_category = doc.xpath("//*[@id=\"zg_listTitle\"]/span").text
+    # カテゴリの表示
+    sub_category = doc.xpath("//*[@id=\"zg_listTitle\"]/span").text
 
-  puts category+"/"+sub_category
+    puts category+"/"+sub_category
+
+    items =  doc.xpath("//div[@id=\"zg_itemRow\"]/div[1]/div[2]")
+    items += doc.xpath("//div[@class=\"zg_itemRow\"]/div[2]/div[2]")
+
+    items.each{ |item|
+      # 順位
+      puts item.xpath("div[1]/span[1]").text
+
+      # 書名
+      puts item.xpath("div[\"zg_title\"]/a").text
+
+      # ASIN
+      puts item.xpath("div[\"zg_title\"]/a").attribute("href").text.match(%r{dp/(.+?)/})[1]
+    }
   end
 end
